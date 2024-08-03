8a605b82b2f4ed5792c18c5914bb71dea4d59929
NguyenDacCuong2604 <20130215@st.hcmuaf.edu.vn>
2024-08-03 23:30:28 +0700

    upload webservice

---
+++ WebService/source/SVM.py
@@ -0,0 +1,25 @@
+from sklearn.svm import SVC
+import joblib
+
+
+class SVM:
+    def __init__(self):
+        self.model = SVC(kernel='rbf', gamma='scale', C=1, probability=True)
+
+    def predict_proba(self, X):
+        probabilities = self.model.predict_proba(X)
+        results = [
+            {str(label): float(prob) for label, prob in zip(self.model.classes_, prob_array)}
+            for prob_array in probabilities
+        ]
+        # Sắp xếp các từ điển theo giá trị prob
+        sorted_results = [
+            dict(sorted(prob_dict.items(), key=lambda item: item[1], reverse=True))
+            for prob_dict in results
+        ]
+
+        return sorted_results
+
+    def load_model(self, path_model):
+        self.model = joblib.load(path_model)
+        return
